da2e17113584d1a49e00e3c638b55ff9bc737226
Joshua Carroll <josh@qntfy.com>
2024-07-25 00:23:23 +0000

    Clean ups

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,7 @@ import numpy as np
 import streamlit as st
 from llama_index.llms.openai import OpenAI
 from llama_index.core import VectorStoreIndex, SimpleDirectoryReader, Settings
+from llama_index.core.chat_engine import CondenseQuestionChatEngine
 from trulens_eval import TruLlama
 from trulens_eval import streamlit as trulens_st
 import tru_st
@@ -35,11 +36,11 @@ if "messages" not in st.session_state:  # Initialize the chat messages history
 messages = st.session_state.messages
 
 @st.cache_resource(show_spinner="Setting up assistant")
-def build_engine():
+def build_query_engine():
     reader = SimpleDirectoryReader(input_dir="./data", recursive=True)
     docs = reader.load_data()
     Settings.llm = OpenAI(
-        model="gpt-3.5-turbo",
+        model="gpt-4o-mini",
         temperature=0.2,
         system_prompt="""You are an expert on 
         the Streamlit Python library and your 
@@ -52,16 +53,25 @@ def build_engine():
     index = VectorStoreIndex.from_documents(docs)
     return index.as_query_engine()
 
-chat_engine = build_engine()
+# Query engine can be shared across all sessions
+query_engine = build_query_engine()
+
+# New chat_engine per session for unique history
+if "chat_engine" not in st.session_state:
+    st.session_state.chat_engine = CondenseQuestionChatEngine.from_defaults(
+        query_engine=query_engine,
+    )
+chat_engine = st.session_state.chat_engine
+
 
 @st.cache_resource(show_spinner="Configuring eval")
 def build_recorder():
     # Initialize provider class
-    provider = tru_OpenAI()
+    provider = tru_OpenAI(model_engine="gpt-4o-mini")
 
     # select context to be used in feedback. the location of context is app specific.
     from trulens_eval.app import App
-    context = App.select_context(chat_engine)
+    context = App.select_context(query_engine)
 
     # Define a groundedness feedback function
     f_groundedness = (
@@ -83,9 +93,9 @@ def build_recorder():
         .aggregate(np.mean)
     )
     # Create the final recorder
-    return TruLlama(chat_engine,
+    return TruLlama(query_engine,
             app_id='LlamaIndex_App1',
-            feedbacks=[f_groundedness, f_answer_relevance, f_context_relevance]
+            feedbacks=[f_groundedness, f_answer_relevance, f_context_relevance],
         )
 
 tru_recorder = build_recorder()
@@ -95,7 +105,7 @@ def draw_eval(record):
         tru_st.trulens_feedback(record=record, key_suffix=record.main_input)
         trulens_st.trulens_trace(record=record)
 
-for message in st.session_state.messages:  # Write message history to UI
+for message in messages:  # Write message history to UI
     with st.chat_message(message["role"]):
         st.write(message["content"])
         if "record" in message:
@@ -103,15 +113,19 @@ for message in st.session_state.messages:  # Write message history to UI
 
 
 if prompt := st.chat_input("Ask a question"):
-    st.session_state.messages.append({"role": "user", "content": prompt})
+    messages.append({"role": "user", "content": prompt})
     st.chat_message("user").write(prompt)
 
     with st.chat_message("assistant"):
         with st.spinner("Thinking..."):
             with tru_recorder as recording:
-                response = chat_engine.query(prompt)
-                st.write(response.response)
-                record = recording.get()
+                response = chat_engine.chat(prompt)
+        record = recording.records[-1]
+        message = {
+                "role": "assistant",
+                "content": response.response,
+                "record": record,
+            }
+        messages.append(message)
+        st.write(response.response)
         draw_eval(record)
-        message = {"role": "assistant", "content": response.response, "record": record}
-        st.session_state.messages.append(message)
